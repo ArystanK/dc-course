493887cd0cdf21aaf4df9dd9eccee309361c91a6
a.kalmakhanov <HbstE25>
2022-10-22 20:36:17 +0900

    Task 2 is complete

--- examples/WebServerKotlin/src/main/kotlin/Processor.kt
+++ examples/WebServerKotlin/src/main/kotlin/Processor.kt
@@ -7,17 +7,33 @@ fun process(socket: Socket, request: HttpRequest) {
     println(request.toString())
     System.out.flush()
 
+    val requestLine = request.requestLine
+
     // To send response back to the client.
     val output = PrintWriter(socket.getOutputStream())
+    when {
+        requestLine.isEmpty() -> printSimplePage(output, listOf("Hello, world!"))
+        requestLine == "create/itemid" -> printSimplePage(output, listOf("Create something with some id"))
+        requestLine == "/delete/itemid" -> printSimplePage(output, listOf("Delete something with some id"))
+        requestLine == "exec/params" -> {
+            printSimplePage(output, listOf("Execute something with some parameters"))
+            Thread.sleep(5000)
+        }
+    }
+    output.flush()
+    socket.close()
+}
 
-    // We are returning a simple web page now.
+fun printSimplePage(output: PrintWriter, content: List<String>) {
     output.println("HTTP/1.1 200 OK")
     output.println("Content-Type: text/html; charset=utf-8")
     output.println()
     output.println("<html>")
     output.println("<head><title>Hello</title></head>")
-    output.println("<body><p>Hello, world!</p></body>")
+    output.println("<body>")
+    content.forEach {
+        output.println(it)
+    }
+    output.println("</body>")
     output.println("</html>")
-    output.flush()
-    socket.close()
 }
